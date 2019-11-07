95525c276a620fc14941c84583a2232fbc85c83f
Samo Makedonija <samomakedonija@protonmail.com>
2019-11-07 18:53:55 +0100

    wip

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function toggle() {
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => chrome.tabs.sendMessage(
+    tabs[0].id, {action: 'toggleDisplay'}
+  ));
+}
+
+document.getElementById('toggle').addEventListener('click', toggle);
